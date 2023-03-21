09711fbed8977334e8b44ad3a13750ced2b5a1ac
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-03-21 12:16:26 +0000

    WS-1.5: Make review changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,21 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-
+  
 
   return (
     <>
       <img src="/logo.png" alt="The logo of the site: a cartoon whale, looking through a pair of binoculars." width="200" />
       <h1>Whale Spotting</h1>
-
+      
       <BackendConnectionChecker />
-      {/* <header><Navbar /></ header>
+
       <Routes>
-        <Route path="/" element=<Home /> />
-        <Route path="/whaleinfo" element=<YYYY /> />
-        <Route path="/leaderboard" element=<YYYY /> />
-        <Route path="/whalesightingviewer" element=<YYYY /> />
-        <Route path="/submitsighting" element=<YYYY /> />
-        <Route path="/speciesid" element=<YYYY /> />
-        <Route path="/planatrip" element=<YYYY /> />
-        <Route path="/login" element=<YYYY /> />
-        <Route path="/signup" element=<YYYY /> />
+        <Route path="/" element={<></>} />
       </Routes>
-      <Footer /> */}
+
     </>
   )
 }
 
 export default App
-
-
-
-
-// const App: React.FunctionComponent = () => {
-
-
