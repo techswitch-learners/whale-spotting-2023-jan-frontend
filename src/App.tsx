64b579644d4d503278c88598ddb6dff1a60a3231
Michael <minimike100@gmail.com>
2023-03-21 15:02:49 +0000

    Homepage-Skeleton updated as per review comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import { BackendConnectionChecker } from "./components/BackendConnectionChecker"
 import { Home } from "./components/Home/Home";
 
 function App() {
-  
-
   return (
     <>
-      <h1></h1>
-      
       <Home />
       <BackendConnectionChecker />
-  
     </>
   )
 }
-
 export default App
