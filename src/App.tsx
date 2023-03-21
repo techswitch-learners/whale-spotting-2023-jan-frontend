c8bfe38e80a44e886c9ffbc38275a36f712d7b1d
Michael <minimike100@gmail.com>
2023-03-21 14:53:23 +0000

    Whale Sighting Detail Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.scss'
 import { BackendConnectionChecker } from "./components/BackendConnectionChecker";
+import { WhaleSightingDetail } from "./components/WhaleSightingDetail/WhaleSightingDetail";
 
 function App() {
-
   return (
     <>
       <img src="/logo.png" alt="The logo of the site: a cartoon whale, looking through a pair of binoculars." width="200" />
       <h1>Whale Spotting</h1>
       <BackendConnectionChecker />
+      <WhaleSightingDetail />
     </>
   )
 }
-
 export default App
