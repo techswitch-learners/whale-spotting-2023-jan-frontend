d14dde179a1a64a25cf309402f60299f3afc4390
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-03-21 14:00:05 +0000

    WS-81-TripPlanner-Skeleton-Desktop

---
+++ src/components/TripPlanner/TripPlanner.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './TripPlanner.scss'
+
+export function TripPlanner() {
+    return (
+        
+        <section className ="tripplanner-container">
+            <div className="tripplanner-header">Please enter your destination for whale spotting!</div>
+            <div className="tripplanner-search">
+            SEARCH
+            </div>
+        <div className="tripplanner-map"> MAP</div>
+        <div className="tripplanner-table">TABLE</div>
+        </section>
+    )
+}
