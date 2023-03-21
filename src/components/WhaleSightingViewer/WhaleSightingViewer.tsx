bac215b25fee6f19cba7706abede44c3ff02e368
Patricia Robles <patriciarobles.tech@gmail.com>
2023-03-21 15:57:38 +0000

    WS-4.1-made review changes

--- src/components/WhaleSightingViewer/WhaleSightingViewer.tsx
+++ src/components/WhaleSightingViewer/WhaleSightingViewer.tsx
@@ -1,26 +1,30 @@
 import "./WhaleSightingViewer.scss";
 
 export function WhaleSightingViewer() {
-	return 	<div>
-			<h2 className="whale-sighting-heading">Whale Sighting Viewer</h2>
-			<div className="whale-sighting-page">
+	return 	<>
+		<h2 className="whale-sighting-heading">Whale Sighting Viewer</h2>
+		<div className="whale-sighting-page">
 			<div className="whale-sighting-map-view-button">Switch to Map View</div>
 			<div className="whale-sighting-filters">Filters to go here</div>
 			<div className="whale-sighting-sort">Sort to go here</div>
-			<div className="whale-sighting-post">
-				<div>
-					<div>Post1 goes here</div>
+			<ul className="whale-sighting-posts">
+				<li className="whale-sighting-post">
+					Post1 goes here
 					<button>Like</button>
-				</div>
-				<div>
-					<div>Post2 goes here</div>
+				</li>
+				<li className="whale-sighting-post">
+					Post2 goes here
 					<button>Like</button>
-				</div>
-				<div>
-					<div>Post3 goes here</div>
+				</li>
+				<li className="whale-sighting-post">
+					Post3 goes here
 					<button>Like</button>
-				</div>
-			</div>
+				</li>
+				<li className="whale-sighting-post">
+					Post4 goes here
+					<button>Like</button>
+				</li>
+			</ul>
 		</div>	
-	</div>
+	</>
 }
