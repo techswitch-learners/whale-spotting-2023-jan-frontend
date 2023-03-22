a692abbd77d694417dde374f96a3b4e5c8024955
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-03-22 15:13:49 +0000

    ws-81-TripPlanner-Skeleton
    Implemented requested changes

--- src/components/TripPlanner/TripPlanner.tsx
+++ src/components/TripPlanner/TripPlanner.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import './TripPlanner.scss'
 
 export function TripPlanner() {
-    return (
-        
-        <section className ="tripplanner-container">
-            <div className="tripplanner-header">Please enter your destination for whale spotting!</div>
-            <div className="tripplanner-search">
+    return <section className="tripplanner-container">
+        <div className="tripplanner-header">
+            Please enter your destination for whale spotting!
+        </div>
+        <div className="tripplanner-search">
             SEARCH
-            </div>
-        <div className="tripplanner-map"> MAP</div>
-        <div className="tripplanner-table">TABLE</div>
-        </section>
-    )
+        </div>
+        <div className="tripplanner-map">
+            MAP
+        </div>
+        <div className="tripplanner-table">
+            TABLE
+        </div>
+    </section>
 }
