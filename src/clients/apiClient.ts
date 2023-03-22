22808549d88a1a2543149314c8b4c940446218a9
Keqi Chen <ckq009@gmail.com>
2023-03-22 15:02:59 +0000

    Create Api call to adding a new sighting

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -1,5 +1,15 @@
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
+export interface NewSighting{
+  dateOfSighting: Date;
+  locationLatitude: number;
+  locationLongitude: number;
+  photoImageURL: string;
+  numberOfWhales: number;
+  description: string;
+  whaleSpecies: string;
+}
+
 export const checkBackendConnection = async (): Promise<boolean> => {
   let response: Response;
   try {
@@ -9,4 +19,18 @@ export const checkBackendConnection = async (): Promise<boolean> => {
   }
   
   return response.ok;
+}
+
+export async function createSighting(newSighting: NewSighting) {
+  const response = await fetch(`https://${backendUrl}/createsighting`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newSighting),
+  });
+
+  if (!response.ok) {
+      throw new Error(await response.json())
+  }
 }
