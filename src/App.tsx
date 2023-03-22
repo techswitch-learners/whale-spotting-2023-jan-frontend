d0915de797ff2456840af6a892fa18326b85ce68
Patricia Robles <patriciarobles.tech@gmail.com>
2023-03-22 15:20:51 +0000

    ws-9.1- created a login component
    created a login componenet with associated scss file, added to routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BackendConnectionChecker } from "./components/BackendConnectionChecker"
 import { Home } from "./components/Home/Home";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Login } from './components/Login/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BackendConnectionChecker />
       <Routes>
         <Route path="/" element={<></>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
