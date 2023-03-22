c831abded456b481fd7f335766186c16bf81b266
James Tribe <jjtribe93@gmail.com>
2023-03-22 10:50:57 +0000

    Tidying
    Some formatting changes; increased height of footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
-import "../Footer.scss"
+import "../Footer.scss";
+import React from "react"
 
-export function Footer() {
-
+export const Footer: React.FC = () => {
     return (
-        <div className = "footer-main">
-                <section className="footer-left"></section>
-                <section className="footer-right"></section>
+        <div className="footer-main">
+            <section className="footer-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt libero id dui posuere ornare. Curabitur metus erat, rutrum at feugiat ac, molestie at enim. Suspendisse potenti. Fusce vel nibh est. Morbi enim sem, maximus vel iaculis id, ullamcorper vel nulla. Aliquam imperdiet elementum dolor, et eleifend ipsum iaculis nec. Proin quis fringilla eros.
+            </section>
+            <section className="footer-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt libero id dui posuere ornare. Curabitur metus erat, rutrum at feugiat ac, molestie at enim. Suspendisse potenti. Fusce vel nibh est. Morbi enim sem, maximus vel iaculis id, ullamcorper vel nulla. Aliquam imperdiet elementum dolor, et eleifend ipsum iaculis nec. Proin quis fringilla eros.
+            </section>
         </div>
     )
 }
