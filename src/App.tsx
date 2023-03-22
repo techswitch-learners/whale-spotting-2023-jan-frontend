8d78a61b44396dc36d6e012fb8c6bdff932e54ee
Prasanti <nalla.prasanti@gmail.com>
2023-03-22 15:20:43 +0000

    made changes based on review comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import './App.scss'
 import { BackendConnectionChecker } from "./components/BackendConnectionChecker";
 import { WhaleSightingDetail } from "./components/WhaleSightingDetail/WhaleSightingDetail";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <img src="/logo.png" alt="The logo of the site: a cartoon whale, looking through a pair of binoculars." width="200" />
       <h1>Whale Spotting</h1>
       <BackendConnectionChecker />
-      <WhaleSightingDetail />
+      <Route>
+        <Route path="/sighting/:id" element={<WhaleSightingDetail />}></Route>
+      </Route>
     </>
   )
 }
--- src/components/WhaleSightingDetail/WhaleSightingDetail.tsx
+++ src/components/WhaleSightingDetail/WhaleSightingDetail.tsx
@@ -1,16 +1,15 @@
 import "./WhaleSightingDetail.scss"
 
 export const WhaleSightingDetail: React.FunctionComponent = () => {
-
-    return (
-        <main>
-            <h1 className="main-header">Whale Sighting Detail Page</h1>
-            <section className="float-container">
-                <div className="float-child1">SightingDetail component 
-                </div>
-                <div className="float-child2">MapDetail component 
-                </div>
-            </section>
-        </main>
-    );
+    return <main>
+        <h1 className="main-header">Whale Sighting Detail Page</h1>
+        <section className="float-container">
+            <div className="float-child1">
+                SightingDetail component
+            </div>
+            <div className="float-child2">
+                MapDetail component
+            </div>
+        </section>
+    </main>
 };
