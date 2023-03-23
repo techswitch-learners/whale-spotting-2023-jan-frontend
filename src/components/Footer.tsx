e2ad2202a466222d289e6473d76c5ca6983ca697
James Tribe <jjtribe93@gmail.com>
2023-03-23 14:05:39 +0000

    Added footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,20 @@ import React from "react"
 export const Footer: React.FC = () => {
     return (
         <div className="footer-main">
-            <section className="footer-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt libero id dui posuere ornare. Curabitur metus erat, rutrum at feugiat ac, molestie at enim. Suspendisse potenti. Fusce vel nibh est. Morbi enim sem, maximus vel iaculis id, ullamcorper vel nulla. Aliquam imperdiet elementum dolor, et eleifend ipsum iaculis nec. Proin quis fringilla eros.
+            <section className="footer-left">
+                <div className="about-site-paragraph">
+                    <p >
+                        WhaleSightings.com is dedicated to creating an online space for the whalewatching community to plan and share sightings of these majestic creatures.
+                    </p>
+                    <p>2023 all rights reserved - website by WhaleSpotting UK Limited</p>
+                </div>
             </section>
-            <section className="footer-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt libero id dui posuere ornare. Curabitur metus erat, rutrum at feugiat ac, molestie at enim. Suspendisse potenti. Fusce vel nibh est. Morbi enim sem, maximus vel iaculis id, ullamcorper vel nulla. Aliquam imperdiet elementum dolor, et eleifend ipsum iaculis nec. Proin quis fringilla eros.
+            <section className="footer-right">
+                <div className="read-more-section">
+                    <p>Read more about whales at:</p>
+                    <p><a href="https://www.nhm.ac.uk/discover/whales.html">https://www.nhm.ac.uk/discover/whales.html</a></p>
+                    <p><a href="https://whalespotting.co.uk/">https://whalespotting.co.uk/</a></p>
+                </div>
             </section>
         </div>
     )
