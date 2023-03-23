6377b47c2ee73301b3d9bdbc6e5a1f256e6a9408
Michael <minimike100@gmail.com>
2023-03-23 11:55:16 +0000

    API call created

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -1,5 +1,12 @@
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
+export interface NewUser {
+  Username: string;
+  Password: string;
+  UserBio: string;
+  profileImageUrl: string;
+}
+
 export const checkBackendConnection = async (): Promise<boolean> => {
   let response: Response;
   try {
@@ -9,4 +16,23 @@ export const checkBackendConnection = async (): Promise<boolean> => {
   }
   
   return response.ok;
+}
+
+export async function createNewUser(newUser: NewUser) {
+  let response: Response;
+  try{
+   response = await fetch(`https://localhost:5173/users/create`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newUser),
+   });
+  }
+  catch {
+    return false;
+  }
+  if (!response.ok) {
+      throw new Error(await response.json())
+  }
 }
