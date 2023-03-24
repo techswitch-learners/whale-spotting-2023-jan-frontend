d6a8b3f8ec14e2330b2645b5638e13f3107f6eca
James Tribe <jjtribe93@gmail.com>
2023-03-24 10:59:59 +0000

    Mobile responsiveness for footer
    Checked mobile responsiveness for footer whih is ok without any media queries; fixed comments from Franc

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,23 +2,23 @@ import "../Footer.scss";
 import React from "react"
 
 export const Footer: React.FC = () => {
-    return (
-        <div className="footer-main">
-            <section className="footer-left">
-                <div className="about-site-paragraph">
-                    <p >
-                        WhaleSightings.com is dedicated to creating an online space for the whalewatching community to plan and share sightings of these majestic creatures.
-                    </p>
-                    <p>2023 all rights reserved - website by WhaleSpotting UK Limited</p>
-                </div>
-            </section>
-            <section className="footer-right">
-                <div className="read-more-section">
-                    <p>Read more about whales at:</p>
-                    <p><a href="https://www.nhm.ac.uk/discover/whales.html">https://www.nhm.ac.uk/discover/whales.html</a></p>
-                    <p><a href="https://whalespotting.co.uk/">https://whalespotting.co.uk/</a></p>
-                </div>
-            </section>
-        </div>
-    )
+    return <div className="footer-main">
+        <section className="footer-left">
+            <div className="about-site-paragraph">
+                <p >
+                    WhaleSightings.com is dedicated to creating an online space for the whalewatching community to plan and share sightings of these majestic creatures.
+                </p>
+                <p>2023 all rights reserved - website by WhaleSpotting Global Limited</p>
+            </div>
+        </section>
+        <section className="footer-right">
+            <div className="read-more-section">
+                <p>Read more about whales at:</p>
+                <a href="https://www.nhm.ac.uk/discover/whales.html">Natural History Museum on Whales</a>
+                <br></br>
+                <br></br>
+                <a href="https://whalespotting.co.uk/">WhaleSpotting UK Site</a>
+            </div>
+        </section>
+    </div>
 }
