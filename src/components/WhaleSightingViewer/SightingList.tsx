fce4909b241295f849f0dab95e4b2534553cdb38
Patricia Robles <patriciarobles.tech@gmail.com>
2023-03-27 16:00:24 +0100

    ws-4.3-sighting-post
    WIP

--- src/components/WhaleSightingViewer/SightingList.tsx
+++ src/components/WhaleSightingViewer/SightingList.tsx
@@ -1,40 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getWhaleSightings, NewSighting } from "../../clients/apiClient";
 import "./SightingList.scss";
 
-export default function SightingList() { 
+export default function SightingList(props: {
+    pageNum: number,
+    sightingsList: NewSighting[],
+}) { 
+
+
+
+    if (!props.sightingsList) return <p>Waiting for data...</p>
+
+    const sightingsToShow = Math.min(props.sightingsList.length, ((props.pageNum-1) * 12 + 12));
 
     const sightings : JSX.Element[] = [];
-
-    // replicate how the data will be returned from the DB and use that in the loop below string[WhaleSighting]
-
-//     public class WhaleSighting
-// {
-//     public int Id { get; set; }
-//     public DateTime DateOfSighting { get; set; }
-//     public float LocationLatitude { get; set; }
-//     public float LocationLongitude { get; set; }
-//     public string PhotoImageURL { get; set; }
-//     public int NumberOfWhales { get; set; }
-//     public ApprovalStatus ApprovalStatus {get;set;}
-//     public string Description { get; set; }
-//     public WhaleSpecies WhaleSpecies { get; set; }
-//     public User User { get; set; }
-//     public ICollection<Like> Likes { get; set; }
-// }
-
-const WhaleSightingurl : string = "https://hips.hearstapps.com/hmg-prod/images/where-to-go-whale-watching-virginia-1522419979.jpg"
     
-    for (let i = 0; i < 12; i++) {
+    for (let i = (props.pageNum-1) * 12; i < sightingsToShow; i++) {
         sightings.push(
             <li className="whale-sighting-post">
+                <Link to={`/sightings/${props.sightingsList[i].Id}`}>
                 <div className="post-container">
-                    <img src={WhaleSightingurl} alt="post image"/>
+                    <img src={props.sightingsList[i].photoImageURL} alt="post image"/>
                     <div className="sighting-info">
-                        <p>date</p>
-                        <p>Username</p>
+                        <div>
+                            <p className="username">{props.sightingsList[i].username}</p>
+                            <p className="date">{props.sightingsList[i].dateOfSighting.toLocaleDateString('en-GB')}</p>
+                        </div>
                         <button>Like</button>
                     </div>
                 </div>
+                </Link>
             </li>);
     }
 
@@ -44,34 +40,3 @@ const WhaleSightingurl : string = "https://hips.hearstapps.com/hmg-prod/images/w
 		</ul>
     </>
 }
-
-
-// export function ImageSelector(props: {
-//     setState: React.Dispatch<React.SetStateAction<string>>,
-//     date: string,
-//     rover: string,
-//     camera: string,
-// }) {
-//     const [apiImages, setApiImages] = useState<imageApiModel[]>();
-
-//     useEffect(() => {
-//         getImagesFromApi(props.date, props.rover, props.camera)
-//             .then(data => setApiImages(data))
-//             .then(() => { if (apiImages) props.setState(apiImages[0].img_src) });
-//     }, [props.date, props.rover, props.camera]);
-
-//     const images = apiImages
-//         ? apiImages.slice(0, Math.min(apiImages.length, 12))
-//             .map(data => <img src={data.img_src}
-//                 key={data.id}
-//                 className="individualImage"
-//                 alt='Mars image'
-//                 onClick={() => props.setState(data.img_src)} />)
-//         : <p>No Image to Display</p>;
-
-//     return <div>
-//         <h4>Select a Photo</h4>
-//         <h5>Showing for the {props.rover} rover's {props.camera} on {props.date}.</h5>
-//         <div className="imageSelector">{images}</div>
-//     </div>
-// }
--- src/components/WhaleSightingViewer/WhaleSightingViewer.tsx
+++ src/components/WhaleSightingViewer/WhaleSightingViewer.tsx
@@ -1,15 +1,33 @@
-import "./WhaleSightingViewer.scss";
+import { useEffect, useState } from "react";
 import SightingList from "./SightingList";
+import { getWhaleSightings, NewSighting } from "../../clients/apiClient";
+import "./WhaleSightingViewer.scss";
 
 export function WhaleSightingViewer() {
+
+	const [page, setPage] = useState(1);
+
+	const [sightingsList, setSightingsList] = useState<NewSighting[]>();
+
+    useEffect(() => {
+        getWhaleSightings()
+            .then(data => setSightingsList(data));
+      }, []);
+
+
+	if (!sightingsList) return <p>Waiting for data...</p>
+
 	return 	<>
 		<h2 className="whale-sighting-heading">Whale Sighting Viewer</h2>
 		<div className="whale-sighting-page">
 			<div className="whale-sighting-map-view-button">Switch to Map View</div>
 			<div className="whale-sighting-filters">Filters to go here</div>
 			<div className="whale-sighting-sort">Sort to go here</div>
-			<SightingList />
+			{<SightingList pageNum = {page} sightingsList = {sightingsList}/>}
+			<div className="page-buttons">
+				{page > 1 ? <button onClick={() => setPage(page - 1)}>Previous</button> :<></>}
+            	{(sightingsList.length > page * 12) ? <button onClick={() => setPage(page + 1)}>Next</button> : <></>}
+	  		</div>
 		</div>
-		</>
+	</>
 }
-
