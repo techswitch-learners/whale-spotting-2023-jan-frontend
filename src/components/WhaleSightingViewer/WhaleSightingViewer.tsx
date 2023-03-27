1929cc7082f17bf03d68f3571d88561d93fb2468
Patricia Robles <patriciarobles.tech@gmail.com>
2023-03-27 10:20:34 +0100

    4.3 creating sighting list -
    work in progress

--- src/components/WhaleSightingViewer/WhaleSightingViewer.tsx
+++ src/components/WhaleSightingViewer/WhaleSightingViewer.tsx
@@ -1,4 +1,5 @@
 import "./WhaleSightingViewer.scss";
+import SightingList from "./SightingList";
 
 export function WhaleSightingViewer() {
 	return 	<>
@@ -7,24 +8,8 @@ export function WhaleSightingViewer() {
 			<div className="whale-sighting-map-view-button">Switch to Map View</div>
 			<div className="whale-sighting-filters">Filters to go here</div>
 			<div className="whale-sighting-sort">Sort to go here</div>
-			<ul className="whale-sighting-posts">
-				<li className="whale-sighting-post">
-					Post1 goes here
-					<button>Like</button>
-				</li>
-				<li className="whale-sighting-post">
-					Post2 goes here
-					<button>Like</button>
-				</li>
-				<li className="whale-sighting-post">
-					Post3 goes here
-					<button>Like</button>
-				</li>
-				<li className="whale-sighting-post">
-					Post4 goes here
-					<button>Like</button>
-				</li>
-			</ul>
-		</div>	
-	</>
+			<SightingList />
+		</div>
+		</>
 }
+
