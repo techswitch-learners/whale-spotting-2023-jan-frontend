6bb691603497135c5f57dc5bbdd1098ee44fd02f
Ying Cheng <yingyingch@yahoo.co.uk>
2023-03-28 11:47:50 +0100

    ws-5.2-sighting-detail-created

---
+++ src/components/SightingDetail/SightingDetail.tsx
@@ -0,0 +1,58 @@
+import "./SightingDetail.scss"
+import React, { useEffect, useState } from 'react';
+import {fetchSighting, WhaleSighting} from "../../clients/apiClient";
+import "./SightingDetail.scss";
+
+
+interface SightingDetailProps {
+    sightingId: string;
+}
+
+export function SightingDetail(props: SightingDetailProps): JSX.Element {
+        const [sighting, setSighting] = useState<WhaleSighting|null>(null); 
+
+        useEffect(() => {
+            fetchSighting(props.sightingId)
+                .then(response => setSighting(response));
+        }, [props]);
+
+        if (!sighting) {
+            return <section>No Sightings found for the provided id</section>
+        }
+    
+    return (
+        <main>
+            <p className="main-header">Sighting Details</p>
+            <section className="sighting-detail-section">
+                <article>
+                    <img className="sighting-image" src={sighting.PhotoImageURL} />
+                    <div className="sighting-info">LocationLatitude : {sighting.LocationLatitude} </div>
+                    <div className="sighting-info">LocationLongitude : {sighting.LocationLongitude} </div>
+                    <div className="sighting-info">DateOfSighting : {sighting.DateOfSighting}</div>
+                    <div className="sighting-info">NumberOfWhales : {sighting.NumberOfWhales}</div>
+                    <div className="sighting-info">Description : {sighting.Description}</div>
+                    <div className="sighting-info">WhaleSpecies : {sighting.WhaleSpecies.Name}</div>
+                    <div className="sighting-info">User : {sighting.User.Username}</div>
+                </article>
+            </section>
+        </main>
+    );
+}
+
+
+
+
+
+
+// public int Id { get; set; }
+//     public DateTime DateOfSighting { get; set; }
+//     public float LocationLatitude { get; set; }
+//     public float LocationLongitude { get; set; }
+//     public string PhotoImageURL { get; set; }
+//     public int NumberOfWhales { get; set; }
+//     public ApprovalStatus ApprovalStatus {get;set;}
+//     public string Description { get; set; }
+//     public WhaleSpecies WhaleSpecies { get; set; }
+//     public User User { get; set; }
+//     public ICollection<Like> Likes { get; set; }
+// }
--- src/components/WhaleSightingDetail/WhaleSightingDetail.tsx
+++ src/components/WhaleSightingDetail/WhaleSightingDetail.tsx
@@ -1,13 +1,19 @@
 import "./WhaleSightingDetail.scss"
+import { SightingDetail } from "../SightingDetail/SightingDetail";
+import { useParams } from "react-router-dom";
 
 export const WhaleSightingDetail: React.FunctionComponent = () => {
+    const { id } = useParams();
+    if (id === undefined) {
+        return <SightingDetail sightingId="0" />
+    }
     return <main>
         <h1 className="main-header">Whale Sighting Detail Page</h1>
         <section className="float-container">
             <div className="float-child1">
-                SightingDetail component
-            </div>
-            <div className="float-child2">
+                <SightingDetail sightingId={id} />
+                </div>
+                <div className="float-child2">
                 MapDetail component
             </div>
         </section>
