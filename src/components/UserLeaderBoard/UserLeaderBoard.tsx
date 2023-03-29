6a313457a463c1a58469324dbc24c2d9d0522733
Keqi Chen <ckq009@gmail.com>
2023-03-29 15:49:42 +0100

    Basic set up for the skeleton

--- src/components/UserLeaderBoard/UserLeaderBoard.tsx
+++ src/components/UserLeaderBoard/UserLeaderBoard.tsx
@@ -3,6 +3,9 @@ import './UserLeaderBoard.scss'
 
 export const UserLeaderBoard: React.FC = () => {
     return <main className="user-leaderboard">
-        <h1>LeaderBoard</h1>
+        <h1 className="leaderboard-header">LeaderBoard</h1>
+        <ol className="leaderboard-list">
+            <li className="leaderboard-item">Data to be populated</li>
+        </ol>
     </main>
 }
