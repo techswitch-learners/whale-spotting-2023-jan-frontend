3c2d6aa4b440802c2786dae8242efcdae0b79b49
James Tribe <jjtribe93@gmail.com>
2023-03-30 14:21:54 +0100

    Created Leaderboard Skeleton
    Table skeleton of leaderboard data which must be populated using API call

--- src/components/UserLeaderBoard/UserLeaderBoard.tsx
+++ src/components/UserLeaderBoard/UserLeaderBoard.tsx
@@ -5,13 +5,23 @@ export const UserLeaderBoard: React.FC = () => {
     return <main className="user-leaderboard">
         <h1 className="leaderboard-header">Leaderboard</h1>
         <table className="leaderboard-table">
-            <tr className="leaderboard-row">
-                <th className="leaderboard-header">Username</th>
-                <th className="leaderboard-header">Number of sightings</th>
-                <th className="leaderboard-header">Likes given</th>
-                <th className="leaderboard-header">Likes received</th>
-                <th className="leaderboard-header">Badges</th>
+            
+            <tr className="leaderboard-header-row">
+                <th className="leaderboard-table-header">Username</th>
+                <th className="leaderboard-table-header">Number of sightings</th>
+                <th className="leaderboard-table-header">Likes given</th>
+                <th className="leaderboard-table-header">Likes received</th>
+                <th className="leaderboard-table-header">Badges</th>
             </tr>
+           
+            <tr className="leaderboard-row">
+                <td className="leaderboard-data">Data to be populated</td>
+                <td className="leaderboard-data">Data to be populated</td>
+                <td className="leaderboard-data">Data to be populated</td>
+                <td className="leaderboard-data">Data to be populated</td>
+                <td className="leaderboard-data">Data to be populated</td>
+            </tr>
+
         </table>
     </main>
 }
