7f86f211a3bda78ee0d39c412688f29d1cf5e1de
Prasanti <nalla.prasanti@gmail.com>
2023-04-04 15:57:02 +0100

    Added whale conservation section on to whale info page

--- src/components/WhaleInfo/WhaleInfo.tsx
+++ src/components/WhaleInfo/WhaleInfo.tsx
@@ -3,6 +3,7 @@ import './WhaleInfo.scss';
 import { WhaleFacts } from "./WhaleFacts";
 import { FamousWhales } from "./FamousWhales";
 import { WhaleInfoSidebar } from "./WhaleInfoSidebar";
+import { WhaleConservation } from "./WhaleConservation";
 
 export const WhaleInfo: React.FC = () => {
     return <main className="whale-info">
@@ -14,22 +15,30 @@ export const WhaleInfo: React.FC = () => {
                 <WhaleInfoSidebar />
             </div>
             <div className="whale-info-main-info">
-            <div  id="whale-facts">
-                <h2 className="whale-info-section-header">
-                    Amazing Whale Facts!
-                </h2>
-                <section className="whale-info-section-contents">
-                    <WhaleFacts />
-                </section>
-            </div>
-            <div id="famous-whales">
-                <h2 className="whale-info-section-header">
-                    Famous Whales!
-                </h2>
-                <section className="whale-info-section-contents">
-                    <FamousWhales />
-                </section>
-            </div>
+                <div id="whale-facts">
+                    <h2 className="whale-info-section-header">
+                        Amazing Whale Facts!
+                    </h2>
+                    <section className="whale-info-section-contents">
+                        <WhaleFacts />
+                    </section>
+                </div>
+                <div id="famous-whales">
+                    <h2 className="whale-info-section-header">
+                        Famous Whales!
+                    </h2>
+                    <section className="whale-info-section-contents">
+                        <FamousWhales />
+                    </section>
+                </div>
+                <div id="whale-conservation">
+                    <h2 className="whale-info-section-header">
+                        Whale Conservation
+                    </h2>
+                    <section className="whale-info-section-contents">
+                        <WhaleConservation />
+                    </section>
+                </div>
             </div>
         </section>
     </main>
